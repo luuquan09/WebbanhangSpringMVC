2907b076a82e939fc000305c3e94141b3c8bb7fd
luuquan09 <63362688+luuquan09@users.noreply.github.com>
2021-03-06 14:37:04 +0700

    2

--- Nienluancoso/src/BEAN/DSmanualinterface.java
+++
@@ -1,9 +0,0 @@
-package BEAN;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public interface DSmanualinterface {
-	public final List<KnapsackPackage> dssp = new ArrayList<KnapsackPackage>();
-	public final List<KnapsackPackageResult> dsspkq = new ArrayList<KnapsackPackageResult>();
-}
